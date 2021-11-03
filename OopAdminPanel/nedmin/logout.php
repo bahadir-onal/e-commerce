2cab166d0c652ce3168d286995f623d871f1330b
bahadir-onal <bahadironal3@gmail.com>
2021-11-03 14:13:06 +0300

    admin panel

--- OopAdminPanel/nedmin/logout.php
+++
@@ -1,7 +0,0 @@
-<?php 
-session_start();
-unset($_SESSION['admins']);
-setcookie("adminsLogin",json_encode($admins),strtotime("-30 day"),"/");
-header("Location:login.php");
-exit;
- ?>
--- OopAdminPanel/nedmin/netting/dbconfig.php
+++
@@ -1,6 +0,0 @@
-<?php 
-define('DBHOST','localhost');
-define('DBUSER','root');
-define('DBPWD','123456789');
-define('DBNAME','ooppanel');
- ?>
